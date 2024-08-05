0d815464c6f5ac9b7decd5e9817d18754008f485
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-08-05 19:23:40 +0000

    add style and media query

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const bar = document.getElementById('menu');
+
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+const hide = document.getElementById('close');
+
+if(hide) {
+    hide.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
